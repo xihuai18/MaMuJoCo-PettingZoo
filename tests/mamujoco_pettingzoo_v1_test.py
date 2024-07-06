c99fb33ce474768db3f4df78bc4149924e44a35f
leoxhwang <1134086740@qq.com>
2024-07-07 01:53:28 +0800

    feat: :alien: update due to co_mas APIs change

--- tests/mamujoco_pettingzoo_v1_test.py
+++ tests/mamujoco_pettingzoo_v1_test.py
@@ -1,4 +1,5 @@
-from pettingzoo.test import parallel_api_test
+from co_mas.test.parallel_api import parallel_api_test, sample_action
+from loguru import logger
 
 from mamujoco_pettingzoo import mamujoco_pettingzoo_v1
 
@@ -17,11 +18,15 @@ obs1, info1 = env1.reset(seed=42)
 obs1_list.append(obs1)
 
 while True:
-    obs1, _, terminated1, _, _ = env1.step({agent: env1.action_space(agent).sample() for agent in env1.agents})
+    obs1, _, terminated1, _, info1 = env1.step(
+        {agent: sample_action(agent, obs1[agent], info1[agent], env1.action_space(agent)) for agent in env1.agents}
+    )
     obs1_list.append(obs1)
 
     if any(terminated1.values()):
         break
+
+env1.close()
 
 env2 = mamujoco_pettingzoo_v1.parallel_env("Ant", "4x2")
 
@@ -30,16 +35,20 @@ obs2, info2 = env2.reset(seed=42)
 obs2_list.append(obs2)
 
 while True:
-    obs2, _, terminated2, _, _ = env2.step({agent: env2.action_space(agent).sample() for agent in env2.agents})
+    obs2, _, terminated2, _, info2 = env2.step(
+        {agent: sample_action(agent, obs2[agent], info2[agent], env2.action_space(agent)) for agent in env2.agents}
+    )
     obs2_list.append(obs2)
 
     if any(terminated2.values()):
         break
 
+env2.close()
+
 for i, (obs1, obs2) in enumerate(zip(obs1_list, obs2_list)):
     assert all(obs1[agent] == obs2[agent] for agent in env1.agents), f"Observations at step {i} differ: {obs1} {obs2}"
 
-print("Seed test passed!")
+logger.success("Seed test passed!")
 
 # Wrapper Tests
 from co_mas.wrappers import AutoResetParallelEnvWrapper, OrderForcingParallelEnvWrapper
@@ -57,7 +66,7 @@ except Exception as e:
         str(e) == "Environment must be reset before stepping"
         or str(e) == "Cannot call env.step() before calling env.reset()"
     ), e
-    print("Order Forcing Test Passed!")
+    logger.success("Order Forcing Test Passed!")
 else:
     raise AssertionError("Expected reset error")
 
@@ -68,18 +77,23 @@ env = mamujoco_pettingzoo_v1.parallel_env(
     additional_wrappers=[OrderForcingParallelEnvWrapper, AutoResetParallelEnvWrapper],
 )
 
-env.reset(seed=42)
+obs, info = env.reset(seed=42)
 
 while True:
-    _, _, terminated, _, _ = env.step({agent: env.action_space(agent).sample() for agent in env.agents})
+    obs, _, terminated, _, info = env.step(
+        {agent: sample_action(agent, obs[agent], info[agent], env.action_space(agent)) for agent in env.agents}
+    )
 
     if all(terminated.values()):
         break
 
-_, _, terminated, _, _ = env.step({agent: env.action_space(agent).sample() for agent in env.agents})
+obs, _, terminated, _, info = env.step(
+    {agent: sample_action(agent, obs[agent], info[agent], env.action_space(agent)) for agent in env.agents}
+)
 
 assert terminated != {agent: True for agent in env.agents}
 
-print("Auto Reset Test Passed!")
+logger.success("Auto Reset Test Passed!")
 
-print("Wrapper Test Passed!")
+logger.success("Wrapper Test Passed!")
+env.close()
